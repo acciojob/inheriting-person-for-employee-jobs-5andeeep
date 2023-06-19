43b024a8816cd4d1dedcddb90db47a77f671935c
Sandeep <newsandeep03@gmail.com>
2023-06-19 12:14:55 +0530

    ans added

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+// greet function added to the Person
+Person.prototype.greet = function(){
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+};
 
-function Employee(name, age, jobTitle) {}
+// created Employee constructor function
+function Employee(name, age, jobTitle) {
+    Person.call(this, name, age); // inheritance property from Person
+    this.jobTitle = jobTitle;  
+}
+
+// Inherit the Person prototype inside the Employee prototype
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function(){
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
+// this is a Person object
+let person = new Person("John", 28);
+person.greet();
+
+// this is Employee object
+let employee = new Employee("Sean", 29, "Manager");
+employee.jobGreet();
 
 // Do not change code below this line
 window.Person = Person;
